34a51dfe9f526bb9e25c449944a008c70b6e4831
Jhan Paul Mackey <jhanmackey2004@ou.edu>
2023-10-27 00:21:23 -0500

    New Update pushed by JP (10/26/23):
    Focused on InventoryManager class:
    TL;DR: Manipulated some vars, exceptions, & code around for easiness,
                 made some methods as additions to original switch methods,
                 changed & tweaked methods to handle updated code.
    
    - Removed push/pull example vars.
    - filename -> lastSavedInventoryFile, assigned to txt relative path.
    - filename2 -> currentSavedInventoryFile, assigned to txt relative path.
    - Created Scanner & BufferedReader/Writer vars for easiness.
    - Initialized Scanner & BufferedReader/Writer vars in needed methods.
    - Created try/catch blocks on exceptions of all methods for easiness.
    - Implemented recursions for needed methods for mismatched user input.
    
    - run(): Added fulfillOrderPrompt on case 2.
    - run(): Added addDonationPrompt on case 3.
    - run(): Tweaked method to handle updated code.
    
    - makeCurrentInventory(): Removed parameters for irrelevance due to
                                                 global file vars.
    - makeCurrentInventory(): Removed for loop & added necessary line in
                                                 while loop for easiness.
    - makeCurrentinventory(): Tweaked method to handle updated code.
    
    - manageInventoryPrompt(): Added saveInventory() on case 6.
    - manageInventoryPrompt(): Tweaked method to handle updated code.
    
    - fulfillOrderPrompt(): Created, similar to manageInventoryPrompt().
    - fulfillOrderPrompt(): Tweaked method to handle updated code.
    
    - addDonationPrompt(): Created, prints example case for functionality.
    
    - saveInventory(): Created, similar to makeInventory().
    - saveInventory(): Tweaked method to handle updated code.
    
    - leaveTerminal(): Changed to where confirmation is needed.
    - leaveTerminal(): Tweaked method to handle updated code.
    
    - endTask(): Since similar to saveInventory, changed to call method.
    - endTask(): Removed parameter as irrelevant due to updated code.

--- src/InventoryManager.java
+++ src/InventoryManager.java
@@ -1,146 +1,248 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InventoryManager {
-    private final String filename = "LastSavedInventory.txt";
-	private final String filename2 = "CurrentInventory.txt";
-    private final String commandSeparator = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+    private final String lastSavedInventoryFile = "OUFB_Inventory_System/LastSavedInventory.txt";
+	private final String currentInventoryFile = "OUFB_Inventory_System/CurrentInventory.txt";
+    
+	private final String commandSeparator = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
     private final String startPrompt = "\n1. Manage Inventory\n2. Fulfill an order\n3. Add a donation\n4. Leave terminal\n> > > ";
+	
 	private ArrayList<String> productNames;
     private ArrayList<Integer> inventoryNumbers;
-	private int ex = 1;
-	private String  ex2 = "wow";
+	private Scanner scan;
+
+	private BufferedReader lastInvReader, currentInvReader;
+	private BufferedWriter lastInvWriter, currentInvWriter;
 
     public InventoryManager() {
         productNames = new ArrayList<String>();
         inventoryNumbers = new ArrayList<Integer>();
-		makeCurrentInventory(filename, filename2);
-    }
+		scan = new Scanner(System.in);
 
-    public void run() throws IOException {
-		doStartPrompt();
+		try {
+			lastInvReader = new BufferedReader(new FileReader(lastSavedInventoryFile));
+			currentInvReader = new BufferedReader(new FileReader(currentInventoryFile));
+			currentInvWriter = new BufferedWriter(new FileWriter(currentInventoryFile));
 
-		Scanner scan = new Scanner(System.in);
-		int input;
-		while((input = scan.nextInt()) != 4){
-			switch(input) {
-				case 1:
-					System.out.println(commandSeparator);
-					manageInventoryPrompt();
-					doStartPrompt();
-					break;
-				case 2:
-					System.out.println(commandSeparator);
-					fufillOrderPrompt();
-					doStartPrompt();
-					break;
-				case 3:
-					addDonationPrompt();
-					doStartPrompt();
-					break;
-				default:
-					if(input != 4){
-						System.out.println(commandSeparator);
-						System.out.print("Invalid input; please provide me with a valid input : ");
-					}
-					break;
-			}
-		};
-		leaveTerminal();
-		scan.close();
-    }
-    
-    public void makeCurrentInventory(String filename, String filename2) {
-        try {
-			BufferedReader reader = new BufferedReader(new FileReader(filename));
-			String line;
-			while((line = reader.readLine()) != null){
-				productNames.add(line.substring(0, line.indexOf(":")));
-				inventoryNumbers.add(Integer.parseInt(line.substring(line.indexOf(":")+2)));
-			}
-			reader.close();
+			makeCurrentInventory();
 
-			BufferedWriter writer = new BufferedWriter(new FileWriter(filename2));
-			for(int i = 0; i < inventoryNumbers.size(); i++){
-				writer.write(productNames.get(i) + ": " + inventoryNumbers.get(i) + System.lineSeparator());
-			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File(s) cannot be found within the computer.");
-		} catch (IOException e) {
-			System.out.println("File(s) cannot be read by the program.");
+		} catch (FileNotFoundException e){
+			System.out.println("File(s) not found in directory/IDE.");
+		} catch (IOException e){
+			System.out.println("File(s) cannot be written in.");
 		}
     }
+
+    public void run() {
+
+		doStartPrompt();
+		int input;
+		try {
+			while((input = scan.nextInt()) != 4) {
+				switch(input) {
+					case 1:
+						System.out.println(commandSeparator);
+						manageInventoryPrompt();
+						doStartPrompt();
+						break;
+					case 2:
+						System.out.println(commandSeparator);
+						fulfillOrderPrompt();
+							doStartPrompt();
+						break;
+					case 3:
+						addDonationPrompt();
+						doStartPrompt();
+						break;
+					default:
+						System.out.println(commandSeparator);
+						System.out.print("Invalid input; please provide me with a valid input : ");
+						break;
+				}
+			}
+
+			leaveTerminal();
+			scan.close();
+
+		} catch (InputMismatchException e) {
+			System.out.println(commandSeparator + "\nInput was not a number. Restarting method...");
+			scan.next();
+			run();
+		}
+    }
+    
+    public void makeCurrentInventory() {
+
+		String line;
+		try {		
+
+			while((line = lastInvReader.readLine()) != null) {
+				productNames.add(line.substring(0, line.indexOf(":")));
+				inventoryNumbers.add(Integer.parseInt(line.substring(line.indexOf(":")+2)));
+
+				currentInvWriter.write(productNames.get(productNames.size()-1) + ": " + inventoryNumbers.get(inventoryNumbers.size()-1) + System.lineSeparator());
+			}
+			lastInvReader.close();
+			currentInvWriter.close();
+
+		} catch (IOException e) {
+			System.out.println("I/O Exception");
+		}
+		
+	}
 
 	public void doStartPrompt(){
         System.out.print(commandSeparator + "\nWhat would you like to do?" + startPrompt);
 	}
 
 	public void manageInventoryPrompt() {
-		Scanner scan = new Scanner(System.in);
+
 		System.out.print("Please select the desired tool:\n1. Show current inventory\n2. Edit product inventory\n3. Add received products\n4. Show target inventory\n5. Edit target inventory\n6. Save inventory\n> > > ");
+		
 		int input;
-		boolean check = true;
-		do {
-			input = scan.nextInt();
-			switch(input) {
-				case 1:
-					System.out.println(commandSeparator + "\nDo something 1");
-					check = false;
+		try {
+
+			while(scan.hasNextInt()) {
+
+				switch(input = scan.nextInt()) {
+					case 1:
+						System.out.println(commandSeparator + "\nDo something 1");
+						break;
+					case 2:
+						System.out.println(commandSeparator + "\nDo something 2"); 
+						break;
+					case 3:
+						System.out.println(commandSeparator + "\nDo something 3"); 
+						break;
+					case 4:
+						System.out.println(commandSeparator + "\nDo something 4"); 
+						break;
+					case 5:
+						System.out.println(commandSeparator + "\nDo something 5"); 
+						break;
+					case 6:
+						saveInventory(); 
+						break;
+					default:
+						System.out.println(commandSeparator);
+						System.out.print("Invalid input; please provide a proper option : ");
+						break;
+				}
+
+				if(input >= 1 && input <= 6) {
 					break;
-				case 2:
-					System.out.println(commandSeparator + "\nDo something 2"); 
-					check = false;
-					break;
-				case 3:
-					System.out.println(commandSeparator + "\nDo something 3"); 
-					check = false;
-					break;
-				case 4:
-					System.out.println(commandSeparator + "\nDo something 4"); 
-					check = false;
-					break;
-				case 5:
-					System.out.println(commandSeparator + "\nDo something 5"); 
-					check = false;
-					break;
-				case 6:
-					System.out.println(commandSeparator + "\nDo something 6"); 
-					check = false;
-					break;
-				default:
-					System.out.print("Invalid input; please provide a valid input : ");	
-					break;
+				}
+
 			}
-		} while(check);
+		} catch (InputMismatchException e) {
+			System.out.println(commandSeparator + "\nInput was not a number. Restarting method...");
+			scan.next();
+			manageInventoryPrompt();
+		}
 	}
 
-	public void fufillOrderPrompt(){
+	public void fulfillOrderPrompt(){
+		System.out.print("Please selected desired tool:\n1. Fulfill physical order\n2. Create online order\n3. Delete online order\n4. View online orders\n > > > ");
+		
+		int input;
+		try {
 
+			while(scan.hasNextInt()) {
+
+				switch(input = scan.nextInt()) {
+					case 1:
+						System.out.println(commandSeparator + "\nDo something 1"); 
+						break;
+					case 2:
+						System.out.println(commandSeparator + "\nDo something 2"); 
+						break;
+					case 3:
+						System.out.println(commandSeparator + "\nDo something 3"); 
+						break;
+					case 4:
+						System.out.println(commandSeparator + "\nDo something 4"); 
+						break;
+					default:
+						if(input < 1 || input > 4) {
+							System.out.println(commandSeparator);
+							System.out.print("Invalid input; please provide a proper option : ");
+						}
+						break;
+				}
+
+				if(input >= 1 && input <= 4) {
+					break;
+				}
+			}
+		} catch (InputMismatchException e) {
+			System.out.println(commandSeparator + "\nInput was not an integer. Restarting method...");
+			scan.next();
+			fulfillOrderPrompt();
+		}
 	}
 
 	public void addDonationPrompt(){
-
+		System.out.println(commandSeparator + "\nDo something about donation");
 	}
 
-	public void leaveTerminal() throws IOException {
-		endTask(inventoryNumbers);
-	}
+	public void saveInventory() {
+		
+		productNames.clear();
+		inventoryNumbers.clear();
 
-	public void exampleChange(){
-		for(int i = 0; i < inventoryNumbers.size(); i++){
-			inventoryNumbers.set(i, inventoryNumbers.get(i)+25);
-			System.out.println(inventoryNumbers.get(i));
+		String line;
+		try {
+			lastInvWriter = new BufferedWriter(new FileWriter(lastSavedInventoryFile));
+
+			while((line = currentInvReader.readLine()) != null){
+				productNames.add(line.substring(0, line.indexOf(":")));
+				inventoryNumbers.add(Integer.parseInt(line.substring(line.indexOf(":")+2)));
+
+				lastInvWriter.write(productNames.get(productNames.size()-1) + ": " + inventoryNumbers.get(inventoryNumbers.size()-1) + System.lineSeparator());
+			}
+			currentInvReader.close();
+			lastInvWriter.close();
+
+			System.out.println(commandSeparator + "\nInventory has been saved.");
+		} catch (IOException e) {
+			System.out.println("File(s) cannot be read by the program.");
+		}
+	}
+	
+	public void leaveTerminal() {
+		System.out.print(commandSeparator + "\nPlease confirm leaving the system (y/n) > > > ");
+		String input;
+		try {
+
+			while(scan.hasNext()) {
+
+				switch(input = scan.next()) {
+					case "y":
+						endTask();
+						break;
+					case "n":
+						run();
+						break;
+					default:
+						System.out.print("Invalid input; please provide a proper option > > > ");
+						break;
+				}
+
+				if(input.equals("y") || input.equals("n")) {
+					break;
+				}
+			}
+		} catch (InputMismatchException e) {
+			System.out.println(commandSeparator + "\nInput was not a letter. Restarting method...");
+			scan.next();
+			leaveTerminal();
 		}
 	}
 
-    public void endTask(ArrayList<Integer> inventoryNumbers) throws IOException {
-		BufferedWriter saveWriter = new BufferedWriter(new FileWriter(filename));
-		for(int i = 0; i < inventoryNumbers.size(); i++){
-			saveWriter.write(productNames.get(i) + ": " + inventoryNumbers.get(i) + System.lineSeparator());
-		}
-		saveWriter.close();
-		System.out.println(commandSeparator + "\nInventory has been saved; safe to end terminal.");
+    public void endTask() {
+		saveInventory();
 	}
 }
